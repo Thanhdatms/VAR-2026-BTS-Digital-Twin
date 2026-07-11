bea13cb4710d2c229c2b4598a5ea6931805b49ba
Thanhdatms <dangthanhdatuit@gmail.com>
2026-07-12 02:23:05 +0700

    feat: add interation

--- scripts/train_public.sh
+++ scripts/train_public.sh
@@ -31,6 +31,6 @@ python3 scripts/train_all.py \
   --iterations "$ITERATIONS" \
   --skip_existing \
   -- \
-  --save_iterations 15000 20000 30000 \
+  --save_iterations 10000 15000 20000 30000 \
   --val_interval 2000 \
   "$@"
